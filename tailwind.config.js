980c3a0f1b615c64fa4be4c34b182c57cf82ee50
“pramudnadula” <pramudn@gmail.com>
2024-09-13 10:23:03 +0530

    Update package.json with tailwindcss dependency and create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ['./src/**/*.{js,jsx,ts,tsx}'];
+export const theme = {
+  extend: {
+    colors: {
+      primaryColor: "#ECDFCC",
+      secondaryColor: "#181C14",
+      tertiaryColor: "#697565",
+      quaternaryColor: "#3C3D37",
+    },
+  },
+};
+export const plugins = [];
+
+
+
