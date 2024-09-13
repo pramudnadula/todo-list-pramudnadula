cea74f2501f08aad4981c12ef1731bd31a71514b
“pramudnadula” <pramudn@gmail.com>
2024-09-13 10:59:10 +0530

    Add routing and context providers to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
+// src/App.tsx
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { TodoProvider } from './context/TodoContext';
+import { Home, Login, Register } from './pages';
 
-
-function App() {
+const App: React.FC = () => {
   return (
-    <div >
-
-    </div>
+    <AuthProvider>
+      <TodoProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </Router>
+      </TodoProvider>
+    </AuthProvider>
   );
-}
+};
 
 export default App;
